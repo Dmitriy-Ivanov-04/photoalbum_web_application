4cedb51f9d313cd525512e636f807dbf4f5050de
htmlka228 <druzh-maks@mail.ru>
2020-05-30 18:26:48 +0400

    Remove not used thymeleaf tags

--- src/main/java/photoalbum/app/web/controller/IndexController.java
+++ src/main/java/photoalbum/app/web/controller/IndexController.java
@@ -7,13 +7,16 @@ import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import photoalbum.app.domain.profile.ProfileService;
 
 @Controller
 public class IndexController {
 	
 	@Autowired
 	MessageSource messageSource;
-	
+
+	@Autowired
+	ProfileService profileService;
 	
 	@GetMapping("/")
 	public String index(Model model, Locale locale) {
