5ba01e71587cc77a4edc11a023e912a7f82f6010
Dmitriy Ivanov <dmitriyivanov2000@yandex.ru>
2020-06-02 02:40:57 +0400

    Made feed the home page

--- src/main/java/photoalbum/app/web/controller/IndexController.java
+++ src/main/java/photoalbum/app/web/controller/IndexController.java
@@ -7,7 +7,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import photoalbum.app.domain.profile.ProfileService;
 
 @Controller
 public class IndexController {
@@ -15,12 +14,11 @@ public class IndexController {
 	@Autowired
 	MessageSource messageSource;
 
-	@Autowired
-	ProfileService profileService;
 	
-	@GetMapping("/feed")
-	public String index() {
+	@GetMapping("/")
+	public String index(Model model, Locale locale) {
 		
+		model.addAttribute("title", messageSource.getMessage("home.title",null, locale));
 		
 		return "feed";
 	}
--- src/main/java/photoalbum/app/web/controller/ProfileController.java
+++ src/main/java/photoalbum/app/web/controller/ProfileController.java
@@ -3,19 +3,13 @@ package photoalbum.app.web.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.relational.core.mapping.MappedCollection;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.servlet.ModelAndView;
 import photoalbum.app.data.ProfileStorage;
-import photoalbum.app.data.profile.ProfileStorageDAO;
-import photoalbum.app.domain.mail.MailSender;
-import photoalbum.app.domain.model.Profile;
 import photoalbum.app.domain.profile.ProfileService;
 import photoalbum.app.web.form.ProfileRegistrationForm;
 import photoalbum.app.web.form.validator.ProfileRegistrationFormValidator;
@@ -32,12 +26,6 @@ public class ProfileController {
 
 	@Autowired
 	ProfileStorage profileStorage;
-
-	/*@Autowired
-	Profile existingUser;*/
-
-	@Autowired
-	private MailSender mailSender;
 
 	@InitBinder("profileForm")
 	private void initBinder(WebDataBinder binder) {
@@ -66,50 +54,4 @@ public class ProfileController {
 		return "redirect:/";
 	}
 
-/*
-	// Display the form
-	@RequestMapping(value="/forgot-password", method=RequestMethod.GET)
-	public ModelAndView displayResetPassword(ModelAndView modelAndView, Profile user) {
-		modelAndView.addObject("profile", user);
-		modelAndView.setViewName("forgotPassword");
-		return modelAndView;
-	}
-
-	// Receive the address and send an email
-	@RequestMapping(value="/forgot-password", method=RequestMethod.POST)
-	public ModelAndView forgotUserPassword(ModelAndView modelAndView, String email) {
-		existingUser = profileService.getProfileByEmail(email);
-		if (existingUser != null) {
-			String messageText = "To complete the password reset process, please click here: "
-					+ "http://localhost:8080/confirm-reset?token=" + existingUser.getToken();
-			// Send the email
-			mailSender.send(existingUser.getEmail(), "Recovery Password", messageText);
-
-			modelAndView.addObject("message", "Request to reset password received. Check your inbox for the reset link.");
-			modelAndView.setViewName("successForgotPassword");
-
-		} else {
-			modelAndView.addObject("message", "This email address does not exist!");
-			modelAndView.setViewName("error");
-		}
-		return modelAndView;
-	}
-
-	// Endpoint to update a user's password
-	@RequestMapping(value = "/reset-password", method = RequestMethod.POST)
-	public ModelAndView resetUserPassword(ModelAndView modelAndView, String email) {
-		existingUser = profileService.getProfileByEmail(email);
-		if (existingUser.getEmail() != null) {
-			// Use email to find user
-			existingUser.setPassword("NewPassword");
-			profileStorage.save(existingUser);
-			modelAndView.addObject("message", "Password successfully reset. You can now log in with the new credentials.");
-			modelAndView.setViewName("successResetPassword");
-		} else {
-			modelAndView.addObject("message","The link is invalid or broken!");
-			modelAndView.setViewName("error");
-		}
-		return modelAndView;
-	}
-	*/
 }
