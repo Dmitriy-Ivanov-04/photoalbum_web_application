022227a9500c6cde4a3ea398d7da7cfadf5952f2
htmlka228 <druzh-maks@mail.ru>
2020-06-12 20:08:36 +0400

    Add new image upload page

--- src/main/java/photoalbum/app/web/controller/PhotoController.java
+++ src/main/java/photoalbum/app/web/controller/PhotoController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +20,7 @@ import org.springframework.web.servlet.ModelAndView;
 import photoalbum.app.domain.model.Profile;
 import photoalbum.app.domain.photo.PhotoServiceDomain;
 import photoalbum.app.domain.profile.ProfileService;
+import photoalbum.app.spring.ProfileDetailsImpl;
 
 @Controller
 @Secured("USER")
@@ -30,11 +33,13 @@ public class PhotoController {
 	private PhotoServiceDomain photoService;
 	
 	@GetMapping("/photo/img-upload")
-    public ModelAndView avatarUpload(ModelAndView modelAndView, @RequestParam("profileId") Optional<Long> profileId) {
+    public ModelAndView avatarUpload(ModelAndView modelAndView, @RequestParam("profileId") Optional<Long> profileId, Model model) {
     	if (profileId.isEmpty()) {
     		modelAndView.setViewName("redirect:/");
     	} else {
     		modelAndView.addObject("profileId", profileId.get());
+            ProfileDetailsImpl profileDetails = (ProfileDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            model.addAttribute("nickname", profileDetails.getNickname());
     		modelAndView.setViewName("/photo/img-upload");
     	}
         return modelAndView;
