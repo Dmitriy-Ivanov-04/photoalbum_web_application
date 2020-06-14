761ebca5841772441819082102e833644ee25baf
htmlka228 <druzh-maks@mail.ru>
2020-06-14 15:07:41 +0400

    Change search

--- src/main/java/photoalbum/app/web/controller/SearchController.java
+++ src/main/java/photoalbum/app/web/controller/SearchController.java
@@ -1,11 +1,13 @@
 package photoalbum.app.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import photoalbum.app.domain.profile.ProfileService;
+import photoalbum.app.spring.ProfileDetailsImpl;
 
 
 @Controller
@@ -13,7 +15,8 @@ public class SearchController {
 
     @GetMapping("/search")
     public String search(Model model) {
-
+        ProfileDetailsImpl profileDetails = (ProfileDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("nickname", profileDetails.getNickname());
         return "resultSearch";
     }
 
--- src/main/resources/static/js/searchScript.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function () {
-	let imageList = $(".publications").find("> img");
-	imageAlign(imageList, $(".publications").width(), 4);
-});
